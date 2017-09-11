beff18e5d131d096aa4f44721d3975860e214711
yamatomurase <yamato.murase@gmail.com>
2017-09-11 18:07:04 +0900

    Update security.py

--- translateApp/translateapp/security.py
+++ translateApp/translateapp/security.py
@@ -1,9 +1,7 @@
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
-
-# from .models import User
-from .models import MyModel
-from .models.users import Users, Phrase
+from .models.models import Users, Phrase
+import bcrypt
 
 
 class MyAuthenticationPolicy(AuthTktAuthenticationPolicy):
@@ -12,11 +10,13 @@ class MyAuthenticationPolicy(AuthTktAuthenticationPolicy):
         if user is not None:
             return user.id
 
+
 def get_user(request):
     user_id = request.unauthenticated_userid
     if user_id is not None:
         user = request.dbsession.query(Users).get(user_id)
         return user
+
 
 def includeme(config):
     settings = config.get_settings()
@@ -27,3 +27,13 @@ def includeme(config):
     config.set_authentication_policy(authn_policy)
     config.set_authorization_policy(ACLAuthorizationPolicy())
     config.add_request_method(get_user, 'user', reify=True)
+
+
+def hash_password(pw):
+    pwhash = bcrypt.hashpw(pw.encode('utf8'), bcrypt.gensalt())
+    return pwhash.decode('utf8')
+
+
+def check_password(pw, hashed_pw):
+    expected_hash = hashed_pw.encode('utf8')
+    return bcrypt.checkpw(pw.encode('utf8'), expected_hash)
