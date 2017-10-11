d1005626da6884a2c4d9e7944e6acdc050aab469
RossyWhite <daikishiroi@gmail.com>
2017-10-11 17:35:49 +0900

    Change password_hash to password

--- translateapp/models/user.py
+++ translateapp/models/user.py
@@ -8,14 +8,14 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(Text, nullable=False)
     email = Column(Text, nullable=False)
-    password_hash = Column(Text, nullable=False)
+    password = Column(Text, nullable=False)
 
     def set_password(self, pw):
         pwhash = bcrypt.hashpw(pw.encode('utf8'), bcrypt.gensalt())
-        self.password_hash = pwhash.decode('utf8')
+        self.password = pwhash.decode('utf8')
 
     def check_password(self, pw):
-        if self.password_hash is not None:
-            expected_hash = self.password_hash.encode('utf8')
+        if self.password is not None:
+            expected_hash = self.password.encode('utf8')
             return bcrypt.checkpw(pw.encode('utf8'), expected_hash)
         return False
